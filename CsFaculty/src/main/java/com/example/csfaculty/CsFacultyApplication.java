2b594ca75441376ca37fb98025231edfef0672c4
VenelinDimitrov <65946212+VenelinDimitrov@users.noreply.github.com>
2022-01-29 11:20:53 +0100

    Created test projects repo and first assigned project CS Faculty

---
+++ CsFaculty/src/main/java/com/example/csfaculty/CsFacultyApplication.java
@@ -0,0 +1,13 @@
+package com.example.csfaculty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsFacultyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsFacultyApplication.class, args);
+	}
+	
+}
---
+++ CsFaculty/src/test/java/com/example/csfaculty/CsFacultyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csfaculty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsFacultyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
